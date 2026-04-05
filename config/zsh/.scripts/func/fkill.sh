7e2769e41fdaccafb6c177bb523be10e3c1b4ed2
david-star-git <david.starinski@proton.me>
2026-04-05 12:53:53 -0300

    moved zsh stuff to home

--- config/zsh/.scripts/func/fkill.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fkill() {
-    local pids
-    pids=$(ps -f -u "$USER" | sed 1d | fzf --height 60% --layout reverse | awk '{print $2}')
-    if [[ -n "$pids" ]]; then
-        echo "$pids" | xargs kill -9 "$@"
-    fi
-}
