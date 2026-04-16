bc8240235e1d20226f041953b5aeb3219c0bc54c
david-star-git <david.starinski@proton.me>
2026-04-15 23:02:04 -0300

    restore waybar

--- config/waybar/scripts/dnd-status.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Outputs waybar return-type=json for the custom/dnd module.
-if makoctl mode | grep -q 'do-not-disturb'; then
-    echo '{"text":"󰂛","tooltip":"Do Not Disturb: On","class":"dnd-on"}'
-else
-    echo '{"text":"","tooltip":"Do Not Disturb: Off","class":"dnd-off"}'
-fi
--- config/waybar/scripts/dnd-toggle.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-# Toggle mako's do-not-disturb mode.
-if makoctl mode | grep -q 'do-not-disturb'; then
-    makoctl mode -r do-not-disturb
-else
-    makoctl mode -a do-not-disturb
-fi
