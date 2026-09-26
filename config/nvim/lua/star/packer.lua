314330c3d8170dfbc89ded588538d5c1e130db0c
david <mail@david-star.com>
2026-09-26 12:46:13 -0300

    removed version from telescope

--- config/nvim/lua/star/packer.lua
+++ config/nvim/lua/star/packer.lua
@@ -18,7 +18,6 @@ return require("packer").startup(function(use)
     use(
     {
         "nvim-telescope/telescope.nvim",
-        tag = "0.1.0",
         requires = { { "nvim-lua/plenary.nvim" } },
     })
     -- ── Colorscheme ───────────────────────────────────────────────────────────
